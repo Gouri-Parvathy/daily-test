bcedb7007bf8cabee77aa23633c08b9a710ec349
Gouri-Parvathy <163575499+Gouri-Parvathy@users.noreply.github.com>
2024-04-30 13:07:15 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,10 @@
+var shape=document.getElementById("shape") 
+var count=document.getElementById("count")
+var counts=1;
+function display(){
+   counts++
+   count.innerHTML="count"+count
+}
+function color(){
+  
+}
